9dcc673a90dfb5aafc726bcb0333255a75991044
Mac <mac@yanzhideMacBook-Air.local>
2018-04-16 18:28:19 +0800

    C指针

--- C&&CPP/数组和指针/main.m
+++ C&&CPP/数组和指针/main.m
@@ -16,12 +16,18 @@ int main(int argc, const char * argv[]) {
         int number[4] = {10,11,12,13};
         
         
+        
+        
+        
+        
         for (int i = 0; i < sizeof(number)/sizeof(int); i ++) {
             int a = number[i];
             printf("%d:---:%p\n",a,&a);
         }
         
-        int *p = (int *)&number;
+        int *p = (int *)&number+1;
+        
+        printf("p- 1 = %d",*(p-1));
         
 
         int b = number[0];
@@ -33,10 +39,20 @@ int main(int argc, const char * argv[]) {
         
         int *  q = (int *)(&number + 1);
         
-        printf("%p\n",q);
+
         
-        printf("%p---%p\n",number,&number);
-        printf("%p---%p\n",number+1,&number+1);
+        printf("%d\n",*(q-1));
+        
+        printf("%p】-1-【%p\n",number,&number);
+        
+        printf("%d】-2-【%d\n",*number,(int)&number);
+        
+        printf("%p】-3-【%p\n",number+1,&number+1);
+        
+        printf("%d】-4-【%p\n",*(number+1),&number+1);
+        
+        
+        
         
     }
     return 0;
